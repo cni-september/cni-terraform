3f55b5499afb01bceb9a24885c838f1ab796a055
root <root@ip-172-31-21-120.eu-west-1.compute.internal>
2018-09-24 01:19:30 +0000

    sg.tf

--- sg.tf
+++ sg.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "allow_ssh1" {
-  name        = "allow-ssh1"
+  name        = "allow-22-80-andalloutbound"
   description = "Allow SSH inbound traffic"
 
   ingress {
@@ -8,23 +8,6 @@ resource "aws_security_group" "allow_ssh1" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_security_group" "allow_outbound1" {
-  name        = "allow-all-outbound1"
-  description = "Allow all outbound traffic"
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "allow_http80" {
-  name        = "allow-80"
-  description = "Allow SSH inbound traffic"
 
   ingress {
     from_port   = 80
@@ -32,5 +15,12 @@ resource "aws_security_group" "allow_http80" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
