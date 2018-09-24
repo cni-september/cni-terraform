aabdfa440e3929ba1dcf96fc77eff1d6b7a2318c
root <root@ip-172-31-21-120.eu-west-1.compute.internal>
2018-09-24 00:45:47 +0000

    adding tf resources

---
+++ aws_ami.tf
@@ -0,0 +1,16 @@
+data "aws_ami" "centos7" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"] 
+}
+
---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "my-test-instance" {
+  ami           = "${data.aws_ami.centos7.id}"
+  instance_type = "t2.small"
+  key_name   = "test_cni"
+
+  tags {
+    Name = "CNI"
+  }
+
+  security_groups = [
+    "${aws_security_group.allow_ssh1.name}",
+    "${aws_security_group.allow_outbound1.name}",
+    "${aws_security_group.allow_http80.name}",
+  ]
+}
