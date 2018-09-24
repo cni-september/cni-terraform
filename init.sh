4bfa338791a4586a4fec291bc0c759866ce23eb4
root <root@ip-172-31-32-32.eu-west-1.compute.internal>
2018-09-24 13:57:33 +0000

    updated init file

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+set -x
 
 sudo su -
 yum install -y docker git
 curl --silent --location http://rpm.nodesource.com/setup_10.x | sudo bash -
 yum install -y nodejs
+
+docker container run -p --d 80:8080 cniseptember/cni-docker
